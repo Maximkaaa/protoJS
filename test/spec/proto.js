164fa15570788adc498e8d6c40a0e3ed96c79d2d
mg <mg@everpoint.ru>
2015-06-22 16:06:35 +0300

    Unit test for plain properties

--- test/spec/proto.js
+++ test/spec/proto.js
@@ -35,5 +35,45 @@ describe('protoJS', function () {
             expect(obj instanceof Class).toBe(true);
             expect(obj.prop).toBe(prototype.prop);
         });
+
+    });
+
+    describe('setting plain value properties', function() {
+
+        var Class, props;
+        beforeEach(function() {
+            Class = function() {};
+            props = {
+                number: 1,
+                text: 'abc',
+                null: null
+            };
+            proto(Class, props);
+        });
+
+        it('should set the properties, specified as plain values', function() {
+            var obj = new Class();
+
+            expect(obj.number).toBe(props.number);
+            expect(obj.text).toBe(props.text);
+            expect(obj.null).toBe(props.null);
+        });
+
+        it('should make the properties enumerable by default', function() {
+            var obj = new Class();
+
+            for (var i in obj) {
+                expect(obj[i]).toBe(props[i]);
+            }
+        });
+
+        it('should make the properties writable', function() {
+            var obj = new Class();
+            obj.number = 2;
+            var obj1 = new Class();
+            expect(obj1.number).toBe(1);
+            expect(obj.number).toBe(2);
+        });
+
     });
 });
